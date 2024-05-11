2ab16e21369dd938c28420608f93effa0f9b281a
fabiansig <fabian.sigmund1@gmail.com>
2024-05-11 02:55:24 +0200

    add buttons

--- chartRendering.js
+++ chartRendering.js
@@ -1,4 +1,5 @@
 import { lifeData, getMaxVal, setMaxVal, loadLifeData, loadData } from './dataProcessing.js';
+
 function init() {
     var w = 800;
     var h = 600;
@@ -32,8 +33,7 @@ function init() {
             country: country.country,
             gdp: country.years[year] ? country.years[year].gdp : null,
             lifeExpec: country.years[year] ? country.years[year].expec : null
-            })).filter(item => item.gdp && item.lifeExpec);
-        console.log("Filtered Data: ", filteredData);
+        })).filter(item => item.gdp && item.lifeExpec);
 
         var xScale = d3.scaleLinear()
             .domain([0, getMaxVal()])
@@ -57,7 +57,7 @@ function init() {
             .on('mouseover', function(event, d) {
                 d3.select('#tooltip')
                     .style('visibility', 'visible')
-                    .html(`Country: ${d.country}<br>Life Expectancy: ${d.lifeExpec}`)
+                    .html(`Country: ${d.country}<br>Life Expectancy: ${d.lifeExpec}<br>GDP: ${d.gdp}`)
                     .style('top', (event.pageY - 10) + 'px')
                     .style('left', (event.pageX + 10) + 'px');
             })
@@ -71,19 +71,26 @@ function init() {
             .attr('cx', d => xScale(d.gdp))
             .attr('cy', d => yScale(d.lifeExpec));
 
-        update.exit().remove();
+        update.exit().remove(); // Ensure elements that no longer exist in data are removed
     }
 
     loadLifeData().then(() => {
         updateChart(document.getElementById('yearSlider').value);
     });
 
-    document.getElementById('csvSelect').addEventListener('change', function () {
-        console.log(this.value);
-        var csvPath = `./data/cleanedData/${this.value}`;
-        setMaxVal(0); // Reset maxGDP
+    document.getElementById('buttonCSV1').addEventListener('click', function () {
+        var csvPath = './data/cleanedData/gdpPerCapita_csv.csv';
+        setMaxVal(0); // Reset maxVal
         loadData(csvPath).then(() => {
-            updateChart(document.getElementById('yearSlider').value, this.options[this.selectedIndex].text);
+            updateChart(document.getElementById('yearSlider').value);
+        });
+    });
+
+    document.getElementById('buttonCSV2').addEventListener('click', function () {
+        var csvPath = './data/cleanedData/gdp_cleaned_csv.csv';
+        setMaxVal(0); // Reset maxVal
+        loadData(csvPath).then(() => {
+            updateChart(document.getElementById('yearSlider').value);
         });
     });
 
